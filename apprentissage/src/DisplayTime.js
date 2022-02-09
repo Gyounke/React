359bbd6f7c9d262fa1f051ca97c6465db3ac8ce6
Kevin <blisteinkevin@outlook.com>
2022-02-09 16:56:00 +0100

    e-shop suite

--- apprentissage/src/DisplayTime.js
+++ apprentissage/src/DisplayTime.js
@@ -1,44 +1,66 @@
-import React from 'react'
+// import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-class DisplayTime extends React.Component {
+// class DisplayTime extends React.Component {
 
-    state = {
-        dateCourante : new Date()
-    };
+//     state = {
+//         dateCourante : new Date()
+//     };
 
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                dateCourante : new Date()
-            })
-        }, 1000)
-    }
+//     componentDidMount() {
+//         setInterval(() => {
+//             this.setState({
+//                 dateCourante : new Date()
+//             })
+//         }, 1000)
+//     }
 
-    render() {
-        let dateCourante = new Date();
-        let tempsJavaScript = this.state.dateCourante.getTime();
-        let tempsPHP = tempsJavaScript / 1000;
+//     render() {
+//         let dateCourante = new Date();
+//         let tempsJavaScript = this.state.dateCourante.getTime();
+//         let tempsPHP = tempsJavaScript / 1000;
 
-        return (
-            <div>
-                { this.props.format === 'jsTime' &&
-                <p>
-                    Temps JavaScript: {tempsJavaScript} millisecondes
-                </p>
-                }
-                { this.props.format === 'phpTime' &&
-                <p>
-                    Temps PHP: {tempsPHP} secondes
-                </p>
-                }
-                { this.props.format === 'humanTime' &&
-                <p>
-                    Date pour humanoïdes: {dateCourante.getDate()}/{dateCourante.getMonth()}/{dateCourante.getFullYear()}
-                </p>
-                }
-            </div>
-        )
-    }
+//         return (
+//             <div>
+//                 { this.props.format === 'jsTime' &&
+//                 <p>
+//                     Temps JavaScript: {tempsJavaScript} millisecondes
+//                 </p>
+//                 }
+//                 { this.props.format === 'phpTime' &&
+//                 <p>
+//                     Temps PHP: {tempsPHP} secondes
+//                 </p>
+//                 }
+//                 { this.props.format === 'humanTime' &&
+//                 <p>
+//                     Date pour humanoïdes: {dateCourante.getDate()}/{dateCourante.getMonth()}/{dateCourante.getFullYear()}
+//                 </p>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+function DisplayTime () {
+    let [tempsJavaScript, setTempsJavaScript] = useState(new Date().getTime());
+
+    setInterval(() => {
+        setTempsJavaScript(new Date().getTime());
+    }, 1000);
+
+    useEffect(() => {
+        document.title = tempsJavaScript;
+    });
+
+    return(
+        <div>
+            <p>
+                Temps Javascript: {tempsJavaScript} millisecondes
+            </p>
+            <button onClick={() => setTempsJavaScript(new Date().getTime())}>Mettre à jour</button>
+        </div>
+    );
 }
 
 export default DisplayTime
